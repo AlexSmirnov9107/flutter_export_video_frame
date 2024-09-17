7d85a0a06e9dc0058304eadeacb7e30e1b67e41d
Alexandr Smirnov <smirnov.a.n.9107@gmail.com>
2024-09-17 12:42:04 +0500

    update media_info

--- test/export_video_frame_test.dart
+++ test/export_video_frame_test.dart
@@ -1,17 +1,17 @@
-import 'package:flutter/services.dart';
+// import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  const MethodChannel channel = MethodChannel('export_video_frame');
+  // const MethodChannel channel = MethodChannel('export_video_frame');
 
   setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
-    });
+    // channel.setMockMethodCallHandler((MethodCall methodCall) async {
+    //   return '42';
+    // });
   });
 
   tearDown(() {
-    channel.setMockMethodCallHandler(null);
+    // channel.setMockMethodCallHandler(null);
   });
 
   // test('getPlatformVersion', () async {
